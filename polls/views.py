321a0c00f4a2ac9ac1b063d4124480d1fefa31bf
FrankApiyo <franklineapiyo@gmail.com>
2020-06-07 15:14:46 +0300

    Hook template to index page view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -6,8 +7,11 @@ from .models import Question
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
